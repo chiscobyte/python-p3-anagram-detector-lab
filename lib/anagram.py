69b05c13c2c4e43341daec09075f8a9dbbe40f86
chiscobyte <chiscoenock@gmail.com>
2025-05-18 18:22:21 +0300

    initiate

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.word_letters = sorted(self.word)
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted(w.lower()) == self.word_letters and w.lower() != self.word]
